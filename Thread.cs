1064f081085d2161fa43f9b314504c4cdacb2513
Venugopal K <venu.shastri@gmail.com>
2020-09-01 15:34:52 +0530

    Create Thread.cs

---
+++ Thread.cs
@@ -0,0 +1,22 @@
+ public class SearchTask
+    {
+        public void Search()
+        {
+
+        }
+    }
+    public class Thread
+    {
+        public void Start()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread _t1 = new Thread();
+            _t1.Start();
+        }
+    }
