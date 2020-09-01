e4448a07baf0b587bf92b5ae65a89f7f224aa340
Venugopal K <venu.shastri@gmail.com>
2020-09-01 16:33:46 +0530

    Update Thread.cs

--- Thread.cs
+++ Thread.cs
@@ -5,18 +5,39 @@
 
         }
     }
+
+    public class ThreadStartCommand
+    {
+        SearchTask _taskObject;
+        public ThreadStartCommand(SearchTask taskObject)
+        {
+            this._taskObject = taskObject;
+        }
+        public void Invoke()
+        {
+            this._taskObject.Search();
+        }
+    }
     public class Thread
     {
+        ThreadStartCommand _command;
+
+        public Thread(ThreadStartCommand command)
+        {
+            this._command = command;
+        }
         public void Start()
         {
-
+            this._command.Invoke();
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Thread _t1 = new Thread();
+            SearchTask _searchTask = new SearchTask();
+            ThreadStartCommand _command = new ThreadStartCommand(_searchTask);
+            Thread _t1 = new Thread(_command);
             _t1.Start();
         }
     }
